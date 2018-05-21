52c3442c0318b4fcc65f8246615711b6167e6e60
Jared Reisinger <jaredreisinger@hotmail.com>
2018-05-21 00:29:03 -0700

    basic functioning service

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # cbp
-Super-simple Golang remote import path service
+
+Super-simple Golang remote import path service.
+
+## Usage
+
+To provide resolution for `go.example.com/...` to a (for instance) Git repository host at `git.example.com` that uses SSH, try:
+
+```sh
+cbp --import-prefix go.example.com --vcs git --repo-prefix ssh://git@git.example.com
+```
+An incoming request for `go.example.com/path/name` will yield:
+
+```
+<meta name="go-import" content="go.example.com/path/name vcs ssh://git@git.example.com/path/name">
+```
+
+## Caveats
+
+* It only handles mapping for a particular import host to a *single* backend repository server.
+
+* It assumes that all repos are "GitHub-style", meaning that there's an owner/organization and a repo name (like "JaredReisinger/cbp", or "mozilla/mig").
+
+On the plus side, because of these restrictions, no advance registration of repositories is needed; the service does not need to be informed/updated when a new repo is created.
+
+## Why "cbp"?
+
+CBP is the common initialism for the U.S. Customs and Border Protection, which deals with customs and importing, and this service is all about custom import paths.
---
+++ main.go
@@ -0,0 +1,103 @@
+package main // import "github.com/JaredReisinger/cbp"
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const addr = ":9090"
+
+const repoInfo = `
+<html>
+  <head>
+    <title>{{ .ImportPath }} - cbp</title>
+	<meta name="go-import" content="{{ .ImportPath}} {{ .VCS }} {{ .RepoPath }}" />
+  </head>
+  <body>
+    <h1>{{ .ImportPath }}</h1>
+	<ul>
+	  <li>VCS: {{ .VCS }}</li>
+	  <li>Repo Root: {{ .RepoPath }}</li>
+	</ul>
+  </body>
+</html>
+`
+
+var (
+	importPrefix string
+	vcs          string
+	repoPrefix   string
+
+	repoTemplate = template.Must(template.New("repoInfo").Parse(repoInfo))
+)
+
+func main() {
+	flag.StringVar(&importPrefix, "import-prefix", "", "the hostname for the custom import path")
+	flag.StringVar(&vcs, "vcs", "", "the VCS for the repos")
+	flag.StringVar(&repoPrefix, "repo-prefix", "", "the actual hosting repo for the custom import path")
+	flag.Parse()
+
+	// ensure any trailing slashes have been removed from importPrefix and
+	// repoPrefix... the calculated paths will *always* start with a slash.
+	importPrefix = strings.TrimRight(importPrefix, "/")
+	repoPrefix = strings.TrimRight(repoPrefix, "/")
+
+	showHelp := false
+
+	if importPrefix == "" {
+		fmt.Println("-import-path required")
+		showHelp = true
+	}
+
+	if vcs == "" {
+		fmt.Println("-vcs required")
+		showHelp = true
+	}
+
+	if repoPrefix == "" {
+		fmt.Println("-repoPrefix required")
+		showHelp = true
+	}
+
+	if showHelp {
+		fmt.Println("")
+		flag.Usage()
+		return
+	}
+
+	log.Printf("Starting cbp at \"%s\"...", addr)
+
+	http.HandleFunc("/", serveMeta)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func serveMeta(w http.ResponseWriter, r *http.Request) {
+	pathPrefix := r.URL.Path
+
+	// Remember that real requests always start with "/", so we ignore that
+	// character.  Also, we split into 3 since we don't use anything past the
+	// second component.
+	parts := strings.SplitN(pathPrefix[1:], "/", 3)
+
+	// For zero, one, or two parts, the existing prefix (path) is good.  For
+	// anything longer, we shorten it to just the first two (org/repo).
+	if len(parts) > 2 {
+		pathPrefix = fmt.Sprintf("/%s", strings.Join(parts[:2], "/"))
+	}
+
+	importPath := fmt.Sprintf("%s%s", importPrefix, r.URL.Path)
+	repoPath := fmt.Sprintf("%s%s", repoPrefix, pathPrefix)
+
+	log.Printf("%s => %s", importPath, repoPath)
+
+	repoTemplate.Execute(w, map[string]string{
+		"ImportPath": importPath,
+		"VCS":        vcs,
+		"RepoPath":   repoPath,
+	})
+}
