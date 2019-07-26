e796e77d2d671554d00be665dd9f4aefbce98f3e
Jared Reisinger <jaredreisinger@hotmail.com>
2019-07-26 15:31:14 -0700

    test: add coverage of serveMeta

--- main_test.go
+++ main_test.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
 	"testing"
 )
 
@@ -63,13 +70,69 @@ func TestValidateOptionsRequired(t *testing.T) {
 		{"x", "x", "/", false},
 	}
 
+	origUsage := flag.Usage
+	defer func() { flag.Usage = origUsage }()
+	flag.Usage = func() {}
+
 	for i, ex := range examples {
 		*importPrefix = ex.importPrefix
 		*vcs = ex.vcs
 		*repoPrefix = ex.repoPrefix
+
 		ok := validateOptions()
 		if ok != ex.ok {
 			t.Errorf("options [%d]: expected %v, got %v", i, ex.ok, ok)
+		}
+	}
+}
+
+type fakeWriter struct {
+	buf bytes.Buffer
+}
+
+func (f *fakeWriter) Header() http.Header {
+	log.Fatal("unexpected")
+	return http.Header{}
+}
+
+func (f *fakeWriter) Write(b []byte) (int, error) {
+	// save output....
+	return f.buf.Write(b)
+}
+
+func (f *fakeWriter) WriteHeader(int) {
+	log.Fatal("unexpected")
+}
+
+func TestServeMeta(t *testing.T) {
+	*importPrefix = "IMPORT"
+	*repoPrefix = "REPO"
+	*vcs = "VCS"
+
+	examples := []struct {
+		requestPath string
+		importPath  string
+		repoPath    string
+	}{
+		{"/foo", "IMPORT/foo", "REPO/foo"},
+		{"/foo/bar", "IMPORT/foo/bar", "REPO/foo/bar"},
+		{"/foo/bar/baz", "IMPORT/foo/bar/baz", "REPO/foo/bar"},
+		{"/foo/bar/baz/quux", "IMPORT/foo/bar/baz/quux", "REPO/foo/bar"},
+	}
+
+	for i, ex := range examples {
+		url, err := url.ParseRequestURI(ex.requestPath)
+		if err != nil {
+			t.Errorf("error in example %d, %q not parsed as a URL", i, ex.requestPath)
+			continue
+		}
+		r := &http.Request{URL: url}
+		w := &fakeWriter{}
+		serveMeta(w, r)
+
+		expectedMeta := fmt.Sprintf("<meta name=\"go-import\" content=\"%s %s %s\" />", ex.importPath, *vcs, ex.repoPath)
+		if !strings.Contains(w.buf.String(), expectedMeta) {
+			t.Errorf("request [%d]: %q did not generate expected meta info %q", i, ex.requestPath, expectedMeta)
 		}
 	}
 }
