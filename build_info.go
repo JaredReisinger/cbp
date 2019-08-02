ab8ecf3de19a3c4ce4b259aab671953e1844ea50
Jared Reisinger <jaredreisinger@hotmail.com>
2019-08-02 14:46:50 -0700

    build(version): add stub build info in preparation for generated build info

---
+++ build_info.go
@@ -0,0 +1,9 @@
+package main
+
+// place holders so that go_get will work.  Do not commit changes, especially
+// the generated one... (can we .gitignore) after the first commit?
+
+const (
+	gitVersion = "(unknown)"
+	buildDate  = "(unknown)"
+)
